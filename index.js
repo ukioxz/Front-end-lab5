f7595938609cd719044c4d8e5d0f0dce930d47be
ukioxz <ikatember@gmail.com>
2022-10-19 20:25:30 +0300

    Add address validation

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let confirmBtn = document.querySelector('.button-confirm');
 let info = document.querySelector('.inputInfo');
 
@@ -8,12 +7,31 @@ confirmBtn.addEventListener('click', (event) => {
   let nameFaculty = document.getElementById('faculty').value;
   let nameAddress = document.getElementById('address').value;
   let nameTelegram = document.getElementById('telegram').value;
+  let testAddress = /^(?!(м\.))*[а-яґєіїА-ЯҐЄІЇ]+$/;
 
+  if (fullName != null && fullName.length < 3 ){
+    alert('Заповніть поле "ПІБ"');
+    return false;
+  } else if (nameGroup != null && nameGroup.length < 4  || nameGroup.length > 4 ){
+  alert('Заповніть назву групи');
+  return false;
+} else if (testAddress.test(nameAddress)){
+alert('Заповніть адресу');
+return false;
+}else {
   document.getElementById("show-name").innerHTML = fullName;
   document.getElementById("show-group").innerHTML = nameGroup;
   document.getElementById("show-faculty").innerHTML = nameFaculty;
   document.getElementById("show-address").innerHTML = nameAddress;
   document.getElementById("show-telegram").innerHTML = nameTelegram;
+}
+
+
+//  document.getElementById("show-name").innerHTML = fullName;
+//  document.getElementById("show-group").innerHTML = nameGroup;
+//  document.getElementById("show-faculty").innerHTML = nameFaculty;
+//  document.getElementById("show-address").innerHTML = nameAddress;
+//  document.getElementById("show-telegram").innerHTML = nameTelegram;
 
   info.style.display = "block";
 })
