93b6efc811f5ed7bd322c9f53bc3f08352a0b8fa
ukioxz <ikatember@gmail.com>
2022-10-19 16:05:22 +0300

    Add all input fields

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ let info = document.querySelector('.inputInfo');
 
 confirmBtn.addEventListener('click', (event) => {
   let fullName = document.getElementById('full_name').value;
-  document.getElementById("name").innerHTML = fullName;
+  let nameGroup = document.getElementById('group').value;
+  let nameFaculty = document.getElementById('faculty').value;
+  let nameAddress = document.getElementById('address').value;
+  let nameTelegram = document.getElementById('telegram').value;
+
+  document.getElementById("show-name").innerHTML = fullName;
+  document.getElementById("show-group").innerHTML = nameGroup;
+  document.getElementById("show-faculty").innerHTML = nameFaculty;
+  document.getElementById("show-address").innerHTML = nameAddress;
+  document.getElementById("show-telegram").innerHTML = nameTelegram;
+
   info.style.display = "block";
-  console.log(fullName)
 })
